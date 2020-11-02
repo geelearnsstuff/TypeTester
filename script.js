56b39dae33b44848f558fb9bc8a3760cb32f4879
GeeLearnsStuff <69557406+geelearnsstuff@users.noreply.github.com>
2020-11-01 18:47:46 -0600

    status save

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 let randomText = document.getElementById("random__text");
-let input2 = document.getElementById("text__ip2");
+
+let genText = document.getElementById("gen__text")
 
 let randArr = ['go', 'know', 'were', 'than', 'time', 'person', 'year', 'way', 'day', 'thing', 'man', 'world', 'life', 'hand', 'part', 'child', 'eye', 'woman', 'place', 'work', 'week', 'case', 'point', 'government', 'company', 'number', 'group', 'problem', 'fact', 'Verbs', 'be', 'have', 'do', 'say', 'get', 'make', 'go', 'know', 'take', 'see', 'come', 'think', 'look', 'want', 'give', 'use', 'find', 'tell', 'ask', 'work', 'seem', 'feel', 'try', 'leave', 'call', 'Adjectives', 'good', 'new ', 'first', 'last', 'long', 'great', 'little', 'own', 'other', 'old', 'right', 'big', 'high', 'different', 'small', 'large', 'next', 'early', 'young', 'important', 'few'];
 
 let randNum = 0;
-let newRandArr = []
+let newRandArr = [];
+let spanElement;
 
 for (let i = 0; i < randArr.length; i++) {
     randNum = Math.floor(Math.random() * randArr.length);
-    newRandArr.push(randArr[randNum])
+    // newRandArr.push(randArr[randNum])
+    spanElement = document.createElement("span");
+    spanElement.className = "word"
+    spanElement.innerText = randArr[randNum]
+    genText.appendChild(spanElement)
 }
 
-randomText.textContent = newRandArr.join(" ")
 
 
-function userInputChanged() {
+function userInputChanged(event) {
 
-    if (randomText === input2.value) {
-        console.log("Its a Match");
-    } else {
-        console.log("It's not a Match")
+    // console.log(event.data)
+    // console.log(event.target)
+    // console.log(event)
+
+    let keyPressed = event.data;
+    let typedWord;
+
+    if (keyPressed === " ") {
+        typedWord = event.target.value.trim();
+        let curWord = newRandArr[0];
+
+
     }
+
 }
