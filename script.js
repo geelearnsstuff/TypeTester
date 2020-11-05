f213d58d3c8a90329dfa3c0e77fcdb838fd7c742
GeeLearnsStuff <69557406+geelearnsstuff@users.noreply.github.com>
2020-11-04 21:12:48 -0600

    status commit

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let genText = document.getElementById("gen__text")
 let randArr = ['go', 'know', 'were', 'than', 'time', 'person', 'year', 'way', 'day', 'thing', 'man', 'world', 'life', 'hand', 'part', 'child', 'eye', 'woman', 'place', 'work', 'week', 'case', 'point', 'government', 'company', 'number', 'group', 'problem', 'fact', 'Verbs', 'be', 'have', 'do', 'say', 'get', 'make', 'go', 'know', 'take', 'see', 'come', 'think', 'look', 'want', 'give', 'use', 'find', 'tell', 'ask', 'work', 'seem', 'feel', 'try', 'leave', 'call', 'Adjectives', 'good', 'new ', 'first', 'last', 'long', 'great', 'little', 'own', 'other', 'old', 'right', 'big', 'high', 'different', 'small', 'large', 'next', 'early', 'young', 'important', 'few'];
 
 let randNum = 0;
-let newRandArr = [];
 let spanElement;
 
 for (let i = 0; i < randArr.length; i++) {
@@ -18,21 +17,35 @@ for (let i = 0; i < randArr.length; i++) {
 }
 
 
-
 function userInputChanged(event) {
 
-    // console.log(event.data)
-    // console.log(event.target)
-    // console.log(event)
+    console.log(event.data)
+    console.log(event.target)
+    console.log(event.target.value)
 
-    let keyPressed = event.data;
     let typedWord;
+
+    document.addEventListener('keyup', e => {
+        if (e.code === 'Space') {
+            typedWord = event.target.value;
+        }
+
+
+    })
 
     if (keyPressed === " ") {
         typedWord = event.target.value.trim();
-        let curWord = newRandArr[0];
-
-
     }
 
 }
+
+
+
+// console.log(genText.children)
+// function highlightWord(index) {
+//     if (index > 0) {
+//         genText.children[index - 1].className = "word";
+//     }
+
+// }
+
