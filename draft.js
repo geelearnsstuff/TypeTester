f56529264f51d59a9c03da7d730119ecc302d7f9
GeeLearnsStuff <69557406+geelearnsstuff@users.noreply.github.com>
2020-11-14 21:35:02 -0600

    implemented timer

--- draft.js
+++ draft.js
@@ -11,18 +11,30 @@ check for match
 1. Style the random text field -DONE
 2. Style the user input area - DONE
 3. Showing more than one word - DONE
-4. checking with the input as the user types
+4. checking with the input as the user types - DONE
 5. Find a way to ensure random generated text area cannot be edited - DONE
-6. Checkout string split and splice to handle string matching
-
 
 **PHASE 3
 ---
-1. Mark the correct words with green
-2. Mark the wrong words with red
-3. Have the current word indicated with a background
-4. Find a better way to store all the words
+1. Mark the correct words with green - DONE
+2. Mark the wrong words with red - DONE
+3. Have the current word indicated with a background - DONE
 
-CREATE A DISCORD ACCOUNT
+**PHASE 4
+---
+1. Style the whole website layout
+2. implement Reset functionality
+3. implement 60second timer
+4. calculate words per minute
+5. hide the input when the timer ends
+6. when page reloads automatically put focus in the input field
+
+
+
+**FINAL REVIEW
+---
+1. Find a better way to store all the words
+2. First letter not printing right..,
+3. Implement themes maybe?(Checkout MDN if statement Documentation you have a simple example at the end.)
 
 */
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let randomText = document.getElementById("random__text");
-
 let genText = document.getElementById("gen__text")
 
 let randArr = ['go', 'know', 'were', 'than', 'time', 'person', 'year', 'way', 'day', 'thing', 'man', 'world', 'life', 'hand', 'part', 'child', 'eye', 'woman', 'place', 'work', 'week', 'case', 'point', 'government', 'company', 'number', 'group', 'problem', 'fact', 'Verbs', 'be', 'have', 'do', 'say', 'get', 'make', 'go', 'know', 'take', 'see', 'come', 'think', 'look', 'want', 'give', 'use', 'find', 'tell', 'ask', 'work', 'seem', 'feel', 'try', 'leave', 'call', 'Adjectives', 'good', 'new ', 'first', 'last', 'long', 'great', 'little', 'own', 'other', 'old', 'right', 'big', 'high', 'different', 'small', 'large', 'next', 'early', 'young', 'important', 'few'];
@@ -51,8 +50,6 @@ function findCurWord(Index) {
         curSpan.className = "word-active";
         curWord = curSpan.innerHTML;
         return curWord;
-    } else {
-        printResults()
     }
 }
 
@@ -61,7 +58,78 @@ function printResults() {
     let para = document.createElement("p");
     para.innerText = `Correct Words : ${correctWordCount} Wrong Words : ${wrongWordCount}`;
     results.appendChild(para)
+    inputField.value = "";
     inputField.disabled = true;
 }
+
+
+let start = document.getElementById("btn__start")
+let counter = document.getElementById("counter__timer")
+
+
+inputField.addEventListener('keyup', startTimer)
+
+
+
+function startTimer(seconds) {
+    let sec = seconds;
+    let mins = sec > 60 ? sec / 60 : 0;
+    let cur_mins;
+
+    function tick() {
+        if (sec > 60) {
+            cur_mins = mins - 1;
+        }
+        sec--;
+        // counter.innerHTML = "00 : " + (sec < 10 ? "0" : "") + sec.toString();
+        counter.innerHTML = "00 : " + (sec < 10 ? "0" : "") + sec.toString();
+
+        (mins > 1 ? ("0" + cur_mins + ":") : ("00 : "))
+
+        console.log(sec)
+
+        if (sec > 0) {
+            setTimeout(tick, 1000) //evaluate expression for every 1000ms or 1 sec
+        } else {
+
+            printResults()
+        }
+    }
+
+    tick();
+}
+
+
+function startTimer(seconds) {
+    inputField.removeEventListener('keyup', startTimer)
+    let sec = 61;
+    let mins = seconds > 60 ? seconds / 60 : 0;
+    let cur_mins;
+
+    function tick() {
+        if (seconds > 60) {
+            cur_mins = mins - 1;
+        }
+        sec--;
+        // counter.innerHTML = "00 : " + (sec < 10 ? "0" : "") + sec.toString();
+        counter.innerHTML = (mins > 1 ? ("0" + cur_mins + ":" + ((sec < 10 ? "0" : "") + sec.toString())) : sec.toString())
+
+        console.log(sec)
+
+        if (sec > 0) {
+            setTimeout(tick, 1000) //evaluate expression for every 1000ms or 1 sec
+        } else if (mins > 1) {
+            startTimer(mins - 1)
+        } else {
+            printResults()
+        }
+    }
+
+    tick();
+}
+
+
+
+
 
 
