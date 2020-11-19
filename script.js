6aa6f6570b5dce63eedb83dc0f56aed0993a5600
GeeLearnsStuff <69557406+geelearnsstuff@users.noreply.github.com>
2020-11-18 20:51:26 -0600

    wpm calculated

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ let spanElement, paraTag, curSpan, nextSpan, curWord, totalSpans, curTopPos, pre
 let correctWordCount, wrongWordCount, curIndex;
 let inputField = document.getElementById('text__ip2');
 let counter = document.getElementById("counter__timer");
+let results = document.getElementById("results");
 let completedSpans = [];
-let keyPressed, typedWord;
+let keyStrokes = 0, keyPressed, typedWord;
 
 //Loading all the required stuff for the Test
 generateText();
@@ -46,6 +47,12 @@ function initialiseTest() {
 //Checks for userinput and takes the whole word when space is hit
 function userInputChanged(event) {
     keyPressed = event.key
+
+    if (keyPressed !== "Backspace" && keyPressed !== "Shift") {
+        console.log("hi")
+        keyStrokes++
+    }
+
 
     if (keyPressed === " ") {
         //trimming the target word as the space is included at the end of input
@@ -108,23 +115,28 @@ function moveUp() {
 
 //printing the results when the timer ends
 function printResults() {
-    let results = document.getElementById("results");
-    let para = document.createElement("p");
-    para.innerText = `Correct Words : ${correctWordCount} Wrong Words : ${wrongWordCount}`;
+    let spans = document.createElement("span");
+    let wpm = calculateWPM()
+    para.innerText = `Correct Words : ${correctWordCount} Wrong Words : ${wrongWordCount},WPM:${wpm}`;
     results.appendChild(para)
     inputField.value = "";
     inputField.disabled = true;
+
+    console.log(keyStrokes)
 }
 
 //Adding an event listener to the input field when the page loads
 function initialiseTimer() {
-    inputField.addEventListener('keyup', function () { startTimer(120) })
 }
+
+inputField.addEventListener('keyup', startTimer);
+
+
 
 //Starting the timer and removing the start timer event 
 function startTimer(seconds) {
-    inputField.removeEventListener('keyup', startTimer)
-    let sec = 60;
+    inputField.removeEventListener('keyup', startTimer);
+    let sec = 10;
     let mins = seconds > 60 ? seconds / 60 : 0;
     let cur_mins;
 
@@ -149,6 +161,8 @@ function startTimer(seconds) {
     tick();
 }
 
+
+
 //TODO: Restarting test functionaly has to be Revisited
 let btn_reset = document.getElementById("reset__btn")
 // btn_reset.addEventListener('click', function () {
@@ -166,8 +180,24 @@ btn_reset.addEventListener('click', function () {
 
 /*
 Calculating Words Per minute.
+=> Rules
+    -- Shift or Backspace not included.
+    -- Spaces should be included
+
+=> Formulae
+    -- Gross WPM = (Total KeyStrokes/5)/Time(min)
+    -- Error Rate =  (Errors (Wrong Words))/Time(min)
+    -- Net WPM = Gross WPM - Error Rate
+    -- Accuracy = (Number of Correct KeyStrokes/Total KeyStrokes)
 
 */
+
+function calculateWPM() {
+    let grossWPM = (keyStrokes / 5) / 1;
+    let errorRate = wrongWordCount / 1;
+    let netWPM = grossWPM - errorRate;
+    return Math.round(netWPM);
+}
 
 
 
